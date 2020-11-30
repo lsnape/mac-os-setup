86f59f9f23dc659ba58d82f2219daf9f8c178b83
Luke Snape <lukesnape@Lukes-MacBook-Pro-2.local>
2020-11-30 16:24:11 +0000

    Remove atom-related files

--- installer/composer.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO set your token and execute the following command:
-#composer config -g github-oauth.github.com your_token_here
-
-composer global require "squizlabs/php_codesniffer=*"
